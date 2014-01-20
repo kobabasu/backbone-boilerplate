117923c9f6c18ee526bc6a5437d58dc3198d4154
kobabasu <keiji@seeknetusa.com>
2014-01-20 09:47:39 +0900

    add changing path script to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+each install command
 npm install
 bundle install --path vendor/bundle
 bower install
 grunt
+
+# change paths to current path in  session.vim
+CURRENT=$(cd $(dirname $0);pwd)
+ESCAPE=`echo $CURRENT | sed "s/\//\\\\\\\\\//g"`
+sed -i -e "s/~\/Dropbox\/personal\/lib\/project/${ESCAPE}/g" session.vim
+rm session.vim-e
+echo "\nsuccessfully change paths to current path in session.vim\n"
