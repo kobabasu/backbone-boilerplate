8f0bc59e44b8411527ab7cdbc97739cc341d92ec
kobabasu <keiji@seeknetusa.com>
2014-01-20 10:11:47 +0900

    add announce at promt when finished change path in session.vim

--- setup.sh
+++ setup.sh
@@ -10,3 +10,4 @@ grunt
 CURRENT=$(cd $(dirname $0);pwd)
 ESCAPE=`echo $CURRENT | sed "s/\//\\\\\\\\\//g"`
 sed -i -e "s/~\/Dropbox\/personal\/lib\/project/${ESCAPE}/g" session.vim
+echo 'successfully change paths to current path in session.vim'
