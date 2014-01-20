7206dd7a5931565bf0c91b85182db057faf80204
kobabasu <keiji@seeknetusa.com>
2014-01-20 10:29:43 +0900

    add newlines

--- setup.sh
+++ setup.sh
@@ -15,4 +15,4 @@ CURRENT=$(cd $(dirname $0);pwd)
 ESCAPE=`echo $CURRENT | sed "s/\//\\\\\\\\\//g"`
 sed -i -e "s/~\/Dropbox\/personal\/lib\/project/${ESCAPE}/g" session.vim
 rm session.vim-e
-echo 'successfully change paths to current path in session.vim'
+echo "\nsuccessfully change paths to current path in session.vim\n"
